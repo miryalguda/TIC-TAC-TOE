0810de2c2710ee784da94db65df72cc92f80600f
miryalguda <137521245+miryalguda@users.noreply.github.com>
2023-11-18 19:24:10 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+//import  Tictactoe  from "./Tictactoe.jsx"
+
+import { startTransition } from "react";
+import Parent from "./Parent.js"
+import Tictactoe from "./Tictactoe.jsx";
+
+
+function App() {
+  return (
+    <div >
+      <Tictactoe />
+    </div>
+  );
+}
+
+export default App;
